a9ae3cf68d62e025a8db9566bf2e6c893bc1a659
JCdotpe <mail@jc.pe>
2015-05-17 01:59:06 -0500

    css

--- application/views/index_view.php
+++ application/views/index_view.php
@@ -17,16 +17,17 @@
         
          <!-- All CSS Styles -->
          <link type="text/css" href="assets/css/bootstrap.css" rel="stylesheet" media="screen" />
-         <link type="text/css" href="assets/css/animate.css" rel="stylesheet" media="screen" />
-         <link type="text/css" href="assets/css/swiper.css" rel="stylesheet" media="screen" />
-         <link type="text/css" href="assets/css/owl.carousel.css" rel="stylesheet" media="screen" />
-         <link type="text/css" href="assets/css/owl.theme.css" rel="stylesheet" media="screen" />
-         <link type="text/css" href="assets/css/magnific-popup.css" rel="stylesheet" media="screen" />
-         <link type="text/css" href="assets/css/styles/style-blue.css" rel="stylesheet" media="screen" />     
-         <link type="text/css" href="assets/css/font-awesome.min.css" rel="stylesheet" media="screen" />
-        
-         <!-- Media Queries -->
-         <link type="text/css" href="assets/css/media.css" rel="stylesheet" media="screen" />
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" />
+
+    <!-- page specific plugin styles -->
+
+    <!-- text fonts -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300" />
+
+    <!-- ace styles -->
+    <link rel="stylesheet" href="http://responsiweb.com/themes/preview/ace/1.3.3/dist/css/ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
+
             
          <!-- Modernizr -->
          <script type="text/javascript" src="assets/js/modernizr.js"></script>     
@@ -39,18 +40,7 @@
                      };            
                  -->
          </script>
-         <style>
-            .slide-menu #logo-mobi a img{
-               position: fixed !important;
-               top: 7px !important;
-               z-index: 9999 !important;
 
-            }
-            .error {
-                 color: red;
-                 font-size:12px;
-            }
-         </style>
         
          <!-- Comments for IE8 and Lower -->                
         
@@ -71,36 +61,226 @@
          <![endif]-->   
       
     </head>
-    <body>
 
-      <h1>MIRAFLOREA</h1>
-       
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">#MIRAFLORES</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Usuario</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid" style="padding-top: 50px">
+      <div class="row">
+
+        <div class="col-sm-4 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Reports</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+            <li><a href="">More navigation</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>
+        </div>
+
+        <div class="col-sm-8 main">
+          <!-- aaa -->
+
+          <h1>CONSTRUCCION DE EDIFICIO LA PLANICIE</h1>
+
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="ace-thumbnails clearfix">
+                <a href="dist/images/gallery/image-4.jpg" data-rel="colorbox" class="cboxElement">
+                   <img height="400" alt="" src="http://i.stack.imgur.com/jGlzr.png">
+              </div>
+            </div>
+          </div>
+
+<div class="widget-box">
+                      <div class="widget-header">
+                        <h4 class="widget-title lighter smaller">
+                          <i class="ace-icon fa fa-comment blue"></i>
+                          Conversation
+                        </h4>
+                      </div>
+
+                      <div class="widget-body">
+                        <div class="widget-main no-padding">
+                          <div class="dialogs ace-scroll"><div class="scroll-track scroll-active" style="display: block; height: 300px;"><div class="scroll-bar" style="height: 236px; top: 64px;"></div></div><div class="scroll-content" style="max-height: 300px;">
+                            <div class="itemdiv dialogdiv">
+                              <div class="user">
+                                <img alt="Alexa's Avatar" src="http://responsiweb.com/themes/preview/ace/1.3.3/dist/avatars/avatar1.png">
+                              </div>
+
+                              <div class="body">
+                                <div class="time">
+                                  <i class="ace-icon fa fa-clock-o"></i>
+                                  <span class="green">4 sec</span>
+                                </div>
+
+                                <div class="name">
+                                  <a href="#">Alexa</a>
+                                </div>
+                                <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque commodo massa sed ipsum porttitor facilisis.</div>
+
+                                <div class="tools">
+                                  <a href="#" class="btn btn-minier btn-info">
+                                    <i class="icon-only ace-icon fa fa-share"></i>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div class="itemdiv dialogdiv">
+                              <div class="user">
+                                <img alt="John's Avatar" src="http://responsiweb.com/themes/preview/ace/1.3.3/dist/avatars/avatar.png">
+                              </div>
+
+                              <div class="body">
+                                <div class="time">
+                                  <i class="ace-icon fa fa-clock-o"></i>
+                                  <span class="blue">38 sec</span>
+                                </div>
+
+                                <div class="name">
+                                  <a href="#">John</a>
+                                </div>
+                                <div class="text">Raw denim you probably haven't heard of them jean shorts Austin.</div>
+
+                                <div class="tools">
+                                  <a href="#" class="btn btn-minier btn-info">
+                                    <i class="icon-only ace-icon fa fa-share"></i>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div class="itemdiv dialogdiv">
+                              <div class="user">
+                                <img alt="Bob's Avatar" src="http://responsiweb.com/themes/preview/ace/1.3.3/dist/avatars/user.jpg">
+                              </div>
+
+                              <div class="body">
+                                <div class="time">
+                                  <i class="ace-icon fa fa-clock-o"></i>
+                                  <span class="orange">2 min</span>
+                                </div>
+
+                                <div class="name">
+                                  <a href="#">Bob</a>
+                                  <span class="label label-info arrowed arrowed-in-right">admin</span>
+                                </div>
+                                <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque commodo massa sed ipsum porttitor facilisis.</div>
+
+                                <div class="tools">
+                                  <a href="#" class="btn btn-minier btn-info">
+                                    <i class="icon-only ace-icon fa fa-share"></i>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div class="itemdiv dialogdiv">
+                              <div class="user">
+                                <img alt="Jim's Avatar" src="http://responsiweb.com/themes/preview/ace/1.3.3/dist/avatars/avatar4.png">
+                              </div>
+
+                              <div class="body">
+                                <div class="time">
+                                  <i class="ace-icon fa fa-clock-o"></i>
+                                  <span class="grey">3 min</span>
+                                </div>
+
+                                <div class="name">
+                                  <a href="#">Jim</a>
+                                </div>
+                                <div class="text">Raw denim you probably haven't heard of them jean shorts Austin.</div>
+
+                                <div class="tools">
+                                  <a href="#" class="btn btn-minier btn-info">
+                                    <i class="icon-only ace-icon fa fa-share"></i>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div class="itemdiv dialogdiv">
+                              <div class="user">
+                                <img alt="Alexa's Avatar" src="http://responsiweb.com/themes/preview/ace/1.3.3/dist/avatars/avatar1.png">
+                              </div>
+
+                              <div class="body">
+                                <div class="time">
+                                  <i class="ace-icon fa fa-clock-o"></i>
+                                  <span class="green">4 min</span>
+                                </div>
+
+                                <div class="name">
+                                  <a href="#">Alexa</a>
+                                </div>
+                                <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+
+                                <div class="tools">
+                                  <a href="#" class="btn btn-minier btn-info">
+                                    <i class="icon-only ace-icon fa fa-share"></i>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </div></div>
+
+                          <form>
+                            <div class="form-actions">
+                              <div class="input-group">
+                                <input placeholder="Type your message here ..." type="text" class="form-control" name="message">
+                                <span class="input-group-btn">
+                                  <button class="btn btn-sm btn-info no-radius" type="button">
+                                    <i class="ace-icon fa fa-share"></i>
+                                    Send
+                                  </button>
+                                </span>
+                              </div>
+                            </div>
+                          </form>
+                        </div><!-- /.widget-main -->
+                      </div><!-- /.widget-body -->
+                    </div>
+
+          <!-- aaa -->
+
+
+        </div>
+      </div>
+    </div>
+
          <!-- Javascript Files -->
          <script type="text/javascript" src="assets/js/jquery-1.11.2.min.js"></script>         
          <script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
-         <script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
-         <script type="text/javascript" src="assets/js/scrollIt.js"></script>         
-         <script type="text/javascript" src="assets/js/swiper.min.js"></script>
-         <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
-         <script type="text/javascript" src="assets/js/jquery.magnific-popup.min.js"></script>
-         <script type="text/javascript" src="assets/js/wow.min.js"></script>
-         <script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
-         <script type="text/javascript" src="assets/js/jquery.stellar.min.js"></script>
-         <script type="text/javascript" src="assets/js/jquery.ajaxchimp.min.js"></script>
-         <script type="text/javascript" src="assets/js/jquery.particleground.min.js"></script>
-         <script type="text/javascript" src="assets/js/custom.js"></script>
+  
 
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-57639106-1', 'auto');
-  ga('send', 'pageview');
-
-</script>         
-                    
-    </body>
 </html>
